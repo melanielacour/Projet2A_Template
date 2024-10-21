a889b7109e6322e5e4817d0796c8d3104ffc509c
Celeste Nenehidini <celeste.nenehidini@eleve.ensai.fr>
2024-10-21 13:19:42 +0200

    upload user_dao

--- src/Model/user_simple.py
+++ src/Model/user_simple.py
@@ -3,7 +3,6 @@ class UserSimple:
         self.id_user = id_user
         self.pseudo = pseudo
         self.is_scout= is_scout
-
     def __repr__(self):
         # Retourner un dictionnaire avec l'ID comme clé et un autre dictionnaire comme valeur
         return str({self.id_user: {"pseudo": self.pseudo, "is_scout": self.is_scout}})
--- src/dao/user_dao.py
+++ src/dao/user_dao.py
@@ -9,12 +9,12 @@ from dao.db_connection import DBConnection
 from Model.user_simple import UserSimple
 
 class UserDao(metaclass=Singleton):
-    def get_all_user(self):
+    def get_all_user_simple(self):
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
                     "SELECT *                   "
-                    "FROM projet_2a.users;     "
+                    "FROM projet_2a.users;      "
         )
         res = cursor.fetchall()
         
@@ -27,8 +27,26 @@ class UserDao(metaclass=Singleton):
                 is_scout=row["is.scout"],
             )
             liste_user.append(user1)
-        return liste_user
+        return {"users": liste_user}
+    
+    def get_user_by_id(self,id):
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT *                   "
+                    "FROM projet_2a.users;      "
+                    "WHERE id_users=%(id)s      ",
+                    {"id": id},
+        )
+        res = cursor.fetchone()
 
+        if res:
+            user1=UserSimple(
+                id_user=row["id"],
+                pseudo=row["pseudo"],
+                is_scout=row["is.scout"],
+            ) 
+        return user1
 
+    #def create_user(self, pseudo, is_scout=False):
 
-  
