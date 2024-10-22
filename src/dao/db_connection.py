e0eddd34183ec94a197c0d6aa2d05183d8f73d65
Melanie Lacour <melanie.lacour@eleve.ensai.fr>
2024-10-22 10:23:33 +0200

    passwordtest

--- src/dao/db_connection.py
+++ src/dao/db_connection.py
@@ -8,11 +8,7 @@ from psycopg2.extras import RealDictCursor
 from src.utils.singleton import Singleton
 
 
-<<<<<<< HEAD:src/dao/DBConnector.py
-class DBConnector(metaclass=Singleton):
-=======
 class DBConnection(metaclass=Singleton):
->>>>>>> 718e5441b4ed547c53f39a7138cfb0e56d2bf412:src/dao/db_connection.py
     """
     Une classe technique pour se connecter avec la base de données
     """
