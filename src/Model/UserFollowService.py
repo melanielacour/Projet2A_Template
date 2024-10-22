6078a74b8e5b23f82a06431d6abb09d2f2897d2a
Baptiste Audrain <baptiste.audrain@ensai.fr>
2024-10-22 10:41:23 +0200

    docs

--- src/Model/UserFollowService.py
+++ src/Model/UserFollowService.py
@@ -5,17 +5,54 @@ class UserFollowService:
         self.scouts = {}  # créer une db scout ?
 
     def become_scout(self, id_user: int) -> None:
-        """Transforme un utilisateur en éclaireur."""
+        """
+        Transforme un utilisateur en éclaireur.
+        
+        Parametres:
+        -----------
+        id_user : int
+            ID de l'utilisateur qui souhaite devenir eclaireur
+
+        Returns:
+        --------
+            None
+        """
         if id_user in self.users:
             self.users[id_user].is_scout = True
             self.scouts[id_user] = []
 
     def add_scout(self, id_user_follower: int, id_scout: int) -> None:
-        """Pour qu'un utilisateur suive un éclaireur."""
+        """
+        Pour qu'un utilisateur suive un éclaireur.
+        
+        Parametres:
+        -----------
+        id_user_follower : int
+            ID de l'utilisateur qui souhaite suivre un scout
+        id_scout : int
+            ID du scout que sera suivi par l'utilisateur
+
+        Returns:
+        --------
+            None
+        
+        """
         if id_user_follower in self.users and id_scout in self.scouts:
             self.scouts[id_user_follower].append(id_scout)
 
     def get_scouts(self, id_user: int) -> list:
-        """Retourne la liste des éclaireurs suivis par l'utilisateur."""
+        """
+        Retourne la liste des éclaireurs suivis par l'utilisateur.
+        
+        Parametres:
+        -----------
+        id_user : int
+            ID de l'utilisateur duquel on retourne la liste de scout
+
+        Returns:
+        --------
+        [self.users[id_scout] for id_scout in self.scouts.get(id_user, [])] : list
+            list de scouts de l'utilisateur
+        """
         return [self.users[id_scout] for id_scout in
                 self.scouts.get(id_user, [])]
