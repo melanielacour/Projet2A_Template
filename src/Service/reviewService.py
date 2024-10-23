af3ef72ba6715598f9f97e1119ce1dbf712b63f3
Cedric Monleon <cedric.monleon@eleve.ensai.fr>
2024-10-23 11:29:57 +0200

    reviewService qui mrche tjrs paaaaassss

--- src/Service/reviewService.py
+++ src/Service/reviewService.py
@@ -1,7 +1,7 @@
 from src.Model.Review import Review
-from src.dao.review.dao import ReviewDAO
+from src.dao.review_dao import ReviewDAO
 
-def add_comment(self,id_user,title,review):
+def add_review(self,id_user,id_film,comment=None,note=None):
     """
         Ajoute un commentaire à un film via la DAO.
 
@@ -9,19 +9,26 @@ def add_comment(self,id_user,title,review):
         -----------
         id_user : int
             L'identifiant de l'utilisateur.
-        title : str
-            Le titre du film.
+        id_film
+            L'identifiant du film.
         comment : str
             Le texte du commentaire à ajouter.
+
+        Retourne:
+        ---------
+
+        bool
         """
-    existing_comments = self.review_dao.get_review_by_user_and_title(id_user, title)
+    review=Review(id_review=1, id_film=id_film,id_user=id_user, comment=comment,note=note)
+    existing_comments = self.review_dao.get_review_by_id_user_and_id_film(id_user, id_film)
 
     if existing_comments:
         return False 
-    result = self.review_dao.add_comment(id_user, title, comment)
-    return result
+    elif note:
+        result = self.review_dao.add_comment(review)
+        return result
 
-def delete_comment(self,id_user,title):
+def delete_review(self,id_user,id_film):
     """
         Supprime un commentaire à un film via la DAO.
 
@@ -29,11 +36,51 @@ def delete_comment(self,id_user,title):
         -----------
 
         id_user : int
-            L'identifiant de l'utilisateur
-        title : str
-            Le titre du film
+            L'identifiant de l'utilisateur.
+        id_film : str
+            L'identifiant du film.
+        
+        Retourne:
+        ---------
+
+        bool
     """
-    existing_comments = self.review_dao.get_review_by_user_and_title(id_user, title)
+    existing_comments = self.review_dao.get_review_by_id_user_and_id_film(id_user, id_film)
 
     if not existing_comments:
-        return False 
+        return False
+    result = self.review_dao.delete_comment(review)
+    return result
+
+def modify_review(self,id_user,id_film, comment_modif, note_modif):
+    """
+        Modifie un commentaire existant.
+        
+        Paramètres:
+        -----------
+
+        id_user : int
+            L'identifiant de l'utilisateur.
+        title : str
+            Le titre du film.
+        comment_modif: str
+            Le nouveau
+        
+        Retourne:
+        ---------
+
+        bool
+    """
+    review=Review(id_review=1, id_film=id_film,id_user=id_user, comment=comment_modif,note=note)
+    existing_comments = self.review_dao.get_review_by_id_user_and_id_film(id_user, id_film)
+
+    if not existing_comments:
+        return False
+    elif not note:
+        return False
+    result = self.review_dao.delete_comment(review)
+    return result
+    result = self.review_dao.add_comment(review)
+    return result
+def delet_comment(u)
+
