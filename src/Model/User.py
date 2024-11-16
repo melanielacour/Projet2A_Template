4711b25491014e4d05ec9fa3f4092de6a1874065
Melanie Lacour <melanie.lacour@eleve.ensai.fr>
2024-11-16 15:47:02 +0100

    text

--- src/Model/User.py
+++ src/Model/User.py
@@ -7,23 +7,27 @@ class User:
     """
     Cette classe représente un utilisateur avec ses attributs (identifiant,
     pseudo, mot de passe, statut d'éclaireur ou non). Elle est utilisée
-    pour gérer les utilisateurs dans l'application.
-
-    Attributs:
-    ----------
-    id: int
-        L'identifiant de l'utilisateur.
-    username: str
-        Le pseudo de l'utilisateur.
-    salt: str
-        Le sel utilisé pour le hachage du mot de passe.
-    password: str
-        Le mot de passe de l'utilisateur.
-    is_scout: bool
-        Indique si l'utilisateur est un éclaireur ou non.
-    """
+    pour gérer les utilisateurs dans l'application. """
 
     def __init__(self, id: int, username: str, salt: str, password: str, is_scout: bool):
+
+        """
+        Cette méthode permet de mettre à jour le statut d'éclaireur (is_scout) d'un utilisateur dans la base de données.
+
+        Parameters :
+        -----------
+        id : int 
+            L'identifiant unique de l'utilisateur.
+        username : str
+            Le pseudo de l'utilisateur.
+        salt : str
+            Le sel utilisé pour le hachage du mot de passe.
+        password : str
+            e mot de passe de l'utilisateur.
+        is_scout : bool
+            Le statut de l'utilisateur, qui indique s'il est un éclaireur ou non.
+        """
+
         # Vérification des types des attributs
         if not isinstance(id, int):
             raise ValidationError("id must be an integer")
@@ -40,10 +44,25 @@ class User:
         self.password = password
         self.is_scout = is_scout
 
+
     def __repr__(self):
+        """
+        Cette méthode retourne une chaîne de caractères représentant l'objet User
+        """
+
         return f"<User(id={self.id}, username='{self.username}', is_scout={self.is_scout})>"
 
+
     def __eq__(self, other):
+        """
+        Cette méthode permet de savoir si deux objets User représentent le même utilisateur en se basant uniquement sur leur identifiant.
+        
+        Parameters :
+        -----------
+        other : 
+            Un autre objet à comparer avec l'objet actuel.
+        """
+
         if isinstance(other, User):
             return self.id == other.id
         return False
--- src/Service/UserService.py
+++ src/Service/UserService.py
@@ -44,19 +44,19 @@ class UserService:
             Si l'identifiant est déjà utilisé ou si le mot de passe ne respecte pas les règles.
         """
 
-        # Vérifier si l'utilisateur existe déjà
+        # on Vérifie si l'utilisateur existe déjà
         pseudo = pseudo.strip()
         password = password.strip()
         if self.user_repo.get_by_username(pseudo):
             raise ValueError("Cet identifiant est déjà utilisé.")
 
-        # Valider la force du mot de passe
+        # on Valide la force du mot de passe
         self.password_service.check_password_strength(password)
 
         # Hacher le mot de passe avant de l'enregistrer
         hashed_password = self.password_service.hash_password(password)
 
-        # Créer le nouvel utilisateur dans la base de données avec le mot de passe haché
+        # on va créer le nouvel utilisateur dans la base de données avec le mot de passe haché
         user_cree = self.user_repo.insert_into_db(username=pseudo, salt= None, hashed_password=hashed_password)
 
         if user_cree:
@@ -90,18 +90,16 @@ class UserService:
         # On récupère le pseudo de l'utilisateur
         user = self.user_repo.get_by_username(pseudo)
 
-        # Vérifie si l'utilisateur existe
+        # on vérifie si l'utilisateur existe
         if not user:
             raise ValueError("Identifiant incorrect.")
 
-        # Vérifier si le mot de passe correspond au pseudo
+        # on vérifie si le mot de passe correspond au pseudo
         if not self.password_service.validate_pseudo_password(pseudo, password):
             raise ValueError("Mot de passe incorrect.")
 
-        # Générer un token JWT
         jwt_response = self.jwt_service.encode_jwt(user.id)
 
-        # Retourner la réponse avec True et le token
         return {"success": True, "token": jwt_response.access_token}
 
     def update_pseudo(self, user_id: int, new_pseudo: str) -> str:
@@ -124,13 +122,14 @@ class UserService:
         if not existing_user:
             raise ValueError("L'utilisateur n'existe pas. Veuillez créer un compte.")
         
-        # Vérifier si le nouveau pseudo est déjà pris
+        # on vérifie si le nouveau pseudo est déjà pris
         if self.user_repo.get_by_username(new_pseudo):
             raise ValueError("Ce pseudo est déjà utilisé.")
-        
-        # Mettre à jour le pseudo
+    
         self.user_repo.update_pseudo(user_id, new_pseudo)
         return "Le pseudo a été mis à jour avec succès."
+
+
     def update_password(self, user_id: int, current_password: str, new_password: str) -> str:
         """
         Permet de changer le mot de passe d'un utilisateur.
@@ -149,7 +148,7 @@ class UserService:
         str
             Message de confirmation si le mot de passe est modifié avec succès.
         """
-        # Vérifier le mot de passe actuel
+        
         user = self.user_repo.get_by_id(user_id)
         if not user:
             raise ValueError("L'utilisateur n'existe pas. Veuillez créer un compte.")
@@ -157,10 +156,8 @@ class UserService:
         if not self.password_service.validate_pseudo_password(user.username, current_password):
             raise ValueError("Mot de passe actuel incorrect.")
 
-        # Vérifier la force du nouveau mot de passe
         self.password_service.check_password_strength(new_password)
 
-        # Mettre à jour le mot de passe avec le nouveau mot de passe haché
         hashed_password = self.password_service.hash_password(new_password)
         self.user_repo.update_password(user_id, hashed_password)
         return "Le mot de passe a été mis à jour avec succès."
@@ -213,4 +210,3 @@ class UserService:
         for val in liste:
             res=FollowerDao(DBConnection).unfollow_scout(id_follower=val, id_scout=user_id)
         return "Votre statut éclaireur a été révoqué."
-
